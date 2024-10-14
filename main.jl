986519126746d62427f5a3af71538c8fe3578c1f
doron <doron@10fs.co.il>
2024-10-14 10:32:51 +0300

    bu

--- main.jl
+++ main.jl
@@ -8,16 +8,18 @@ function main()
   α = 50.0
   local integ =[0.0, 0.0, 0.0, v0*cosd(α), 0.0, v0*sind(α), missile.propMass]
   local t = 0.0
-  local step = 0.01
-  #typeof(integ)
+  local step = 0.001
   idx=0
-  f = open("output.txt", "w") 
+  f = open("output.txt", "w")
+  write(f,"t x y z vx vy vz pmass")
+  write(f, "\n")
   while true
-    missile.Integrate(t, step, false, integ)
-    fmtd = map(x ->@sprintf("%f", x), integ)
-    write(f, join(fmtd, ", "))
-    write(f, "\n")
+    missile.Integrate(t, step, integ)
     t += step
+    prtv = [[t] ; integ]
+    fmtd = map(x ->@sprintf("%f", x), prtv)
+    write(f, join(fmtd, " "))
+    write(f, "\n")
     if (integ[3]<0.0 && integ[6]<0.0) || t>100
       break
     end
@@ -27,3 +29,4 @@ function main()
 end
 
 main()
+println("*** finished ****")
--- missile.jl
+++ missile.jl
@@ -1,10 +1,12 @@
 module missile
+using Printf
 
-export propMass, tburn
 export Integrate
 
-propMass = 1000.0
+propMass = 2000.0
 inertMass = 1000.0
+thrust = 1000.0
+
 #tburn = 107.1
 const vecSz = 7
 
@@ -20,24 +22,24 @@ Coeff() = coeff(0.0, 0.0, 0.0, 0.0)
 #rkCoeffs = Array{coeff, 1}(undef, 6)
 rkCoeffs = [Coeff() for _ in 1:7]
 
-function ffunc(t::Float64, integ::Vector{Float64})::Vector{Float64}
+function ffunc(t::Float64, integ::Vector{Float64})
   gamma = atan(integ[6], integ[4])
   engOn = integ[7] > 0.0
+  # str = @sprintf("%f %d", t, engOn ? 1 : 0)
+  # println(str)
   if engOn
     g =  0.0
-    thrust = 100.0
-    mdot= -1000.0 / 106.9
+    mdot= -propMass / 106.9
     acc = thrust / (inertMass + integ[7])
   else
     g =  -9.81
-    thrust = 0.0
     mdot= 0.0
     acc = 0.0
   end  
   deriv =[integ[4], integ[5], integ[6], acc*cos(gamma), 0.0, acc*sin(gamma) + g, mdot]
 end
 
-function Integrate(t0::Float64, step::Float64, exact::Bool, integ::Vector{Float64})
+function Integrate(t0::Float64, step::Float64, integ::Vector{Float64})::Vector{Float64}
   hv = integ
   # step 1
   deriv=ffunc(t0, hv)
@@ -63,6 +65,7 @@ function Integrate(t0::Float64, step::Float64, exact::Bool, integ::Vector{Float6
     rkCoeffs[idx].k4 = deriv[idx]*step
     integ[idx] = integ[idx] + (rkCoeffs[idx].k1 + 2.0 * rkCoeffs[idx].k2 + 2.0 * rkCoeffs[idx].k3 + rkCoeffs[idx].k4) / 6.0;
   end
+  integ
 end
 
 
